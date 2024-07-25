f25585235c04909cfe38004bb298fdda000149a2
Pratik Dhame <96607747+pratiktechie@users.noreply.github.com>
2024-07-25 08:40:20 +0530

    ignited our React app using Parcel

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
 const parent= React.createElement(
     "div",
     {id: "parent"}, 
-    React.createElement(
-        "div", 
-        {id: "child"}, 
-       [ React.createElement("h1",{},"Im an child tag 1"), 
-        React.createElement("h1",{},"Im an child tag 2"), 
+    React.createElement("div", {id: "child"}, 
+    [ React.createElement("h1",{},"This is Namste Javascript🚀🚀🚀"), 
+      React.createElement("h1",{},"Im an child tag 2"), 
     ]),
-    React.createElement(
-        "div", 
-        {id: "child"}, 
-       [ React.createElement("h1",{},"Im an child tag 1"), 
-        React.createElement("h1",{},"Im an child tag 2"), 
+    React.createElement("div",{id: "child2"}, 
+    [ React.createElement("h1",{},"Im an child tag 1"), 
+      React.createElement("h1",{},"Im an child tag 2"), 
     ])
 );
 
---
+++ README.md
@@ -0,0 +1,23 @@
+...
+# Namaste React 🚀
+...
+
+# parcel
+- Dev Build
+- Local Server
+- HMR = Hot module Replacement
+- Caching - Faster Builds
+- Image Optimization
+- Minification of file
+- Bundling 
+- Compress
+- consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
+
+
