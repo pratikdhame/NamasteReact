9845548261406de304864245506648db67327128
Pratik Dhame <pratikdhamepawar@gmail.com>
2024-08-05 23:34:07 +0530

    ep 6 till 1h34m

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData.js";
+import Shimmer from "./Shimmer.js";
 
 const Body = () => {
   // State Variable - Super Powerful variable
   // Changes Github Ownership
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=18.61610&lng=73.72860&carousel=true&third_party_vendor=1"
+    );
+
+    const json = await data.json();
+    setListOfRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer></Shimmer>
+  ) : (
     <div className="body">
       <div className="filter">
         <button
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 import image_logo from "../../public/images/site_Logo_jpeg.jpg";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  const handleClick = () => {
+    btnName === "Logout" ? setBtnName("Login") : setBtnName("Logout");
+  };
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -19,6 +26,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login-btn" onClick={handleClick}>
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
